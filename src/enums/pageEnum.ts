a20d7c8f1388c81cc8c17b56a96948346d450a82
BreathlessWay <731005087@qq.com>
2024-12-02 11:26:39 +0800

    style: update

---
+++ src/enums/pageEnum.ts
@@ -0,0 +1,14 @@
+export enum PageEnum {
+  // 登录
+  BASE_LOGIN = '/login',
+  BASE_LOGIN_NAME = 'Login',
+  //重定向
+  REDIRECT = '/redirect',
+  REDIRECT_NAME = 'Redirect',
+  // 首页
+  BASE_HOME = '/data',
+  //首页跳转默认路由
+  BASE_HOME_REDIRECT = '/data/analyze/search',
+  // 错误
+  ERROR_PAGE_NAME = 'ErrorPage',
+}
